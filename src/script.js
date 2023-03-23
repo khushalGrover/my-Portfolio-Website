102ea7583890b5d4dc85d8a9e375b2bd1d219202
khushalGrover <khushal15grover@gmail.com>
2023-03-24 00:11:13 +0530

    many adjustments

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $(window).scroll(function(){
-        if(this.scrollY> 20){
+        if(this.scrollX> 800){
             $('.navbar').addClass("sticky");
         }else{
             $('.navbar').removeClass("sticky");
