fc335308cce69a98cced43571ccbd6882a0e2664
khushalGrover <khushal15grover@gmail.com>
2023-06-15 21:51:29 +0530

    clean up

--- src/javaScript/script.js
+++
@@ -1,8 +0,0 @@
-const element = document.getElementsByTagName("main")[0];
-
-element.addEventListener('wheel', (event) => {
-  event.preventDefault();
-  element.scrollBy({
-    left: event.deltaY < 0 ? -30 : 30,
-  });
-});
