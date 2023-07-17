1af2aeaf149825f528f7951a002127a1a8065953
khushalGrover <khushal15grover@gmail.com>
2023-07-17 13:29:32 +0530

    replace character to testMonkeyHead
    in hero section change bg and replace 3d model character to mokey head.

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,7 +5,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./character/kg-character-v2.0_004.gltf");
+  const computer = useGLTF("./monkeyHead/animMonkey.gltf");
 
   return (
     <mesh>
@@ -21,9 +21,12 @@ const Computers = ({ isMobile }) => {
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 8}
-        position={isMobile ? [0, -3, -2.2] : [-10, -8, 5]}
-        rotation={[0, Math.PI, 0]}
+        // scale={isMobile ? 0.7 : 8}
+        scale={8}
+
+        // position={isMobile ? [0, -3, -2.2] : [-10, -8, 5]}
+        position={[-10, 0, 5]}
+        rotation={[0, (Math.PI + 0.001 ), 0]}
       />
     </mesh>
   );
